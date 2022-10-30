6f1f7d65c1dfeddcad7707b7a512a6131d28eff4
Vlad <Smirnov.ru92@mail.ru>
2022-10-30 21:30:55 +0500

    Updates

---
+++ precache-manifest.e9c9d017771d0093a8e3455bc96b84e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8bed2d93e7d01daa75e7e8b4dafa992",
+    "url": "/namerepository/index.html"
+  },
+  {
+    "revision": "43537451e5828423e4e6",
+    "url": "/namerepository/static/css/main.d86ab26f.chunk.css"
+  },
+  {
+    "revision": "36a3e56275a9e65a8815",
+    "url": "/namerepository/static/js/2.75988428.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/namerepository/static/js/2.75988428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43537451e5828423e4e6",
+    "url": "/namerepository/static/js/main.0797fc2c.chunk.js"
+  },
+  {
+    "revision": "9e079438edc04652f532",
+    "url": "/namerepository/static/js/runtime-main.f3e25a82.js"
+  }
+]);
