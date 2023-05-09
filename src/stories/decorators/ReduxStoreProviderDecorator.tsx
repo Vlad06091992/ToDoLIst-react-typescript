49deb206886210c80a680f4f0ecdd80a0688c09a
Vlad <Smirnov.ru92@mail.ru>
2023-05-09 09:28:31 +0500

    first commit

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Provider } from 'react-redux';
-// @ts-ignore
-import {store} from "src/app/store";
-// import { store } from './store';
-
-export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
-    return <Provider store={store}>{storyFn()}</Provider>
-}
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // ...
-    resolve: {
-        test: /\.m?js/,
-        fullySpecified: false,
-    },
-};
